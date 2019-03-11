08e9c477f7bc9d03e35c2eda273629b4705a6f9d
kenithgrey <kenithgrey@r-bar.net>
2019-03-11 11:52:02 -0400

    Added License information to files

--- R/ListModels.R
+++ R/ListModels.R
@@ -1,6 +1,22 @@
-#########################
-# Get Models and Inputs #
-#########################
+##############################
+# Copyright 2019 Kenith Grey #
+##############################
+
+# Copyright Notice --------------------------------------------------------
+# This file is part of MobileTrigger.
+#
+# MobileTrigger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MobileTrigger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ggQC.  If not, see <http://www.gnu.org/licenses/>.
 
 #' @export
 #' @import caret
